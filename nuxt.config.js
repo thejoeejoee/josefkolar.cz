26d077ee0e319f07688d41c4e97f95b9a0872d73
Josef Kolář <thejoeejoee@gmail.com>
2020-06-08 22:36:47 +0300

    Voilá, simplified skeleton

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,10 +49,9 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: colors.purple.darken1,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
